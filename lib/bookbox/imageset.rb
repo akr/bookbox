f55f16179717a0b5404bf266f3e90f74b9680408
Tanaka Akira <akr@fsij.org>
2011-03-31 00:01:50 +0900

    basic "make" feature implemented.

--- lib/bookbox/imageset.rb
+++ lib/bookbox/imageset.rb
@@ -1,8 +1,45 @@
 require 'open3'
+require 'json'
+require 'fileutils'
 
 class ImageSet
   def initialize(dir)
     @dir = dir
+    scan_json_path = "#{dir}/scan.json"
+    if File.exist? scan_json_path
+      @index_hash = File.open(scan_json_path) {|f| JSON.load(f) }
+    else
+      @index_hash = {}
+    end
+    @index_hash["list"] ||= image_filenames
+    @index_hash["i"] ||= {}
+  end
+
+  def mktmppath(suffix='')
+    n = 1
+    begin
+      path = "#{@dir}/tmp#{n}#{suffix}"
+      f = File.open(path, File::CREAT|File::WRONLY|File::EXCL)
+    rescue Errno::EEXIST
+      n += 1
+      retry
+    end
+    f.close
+    if block_given?
+      begin
+        yield path
+      ensure
+        File.delete path
+      end
+    else
+      path
+    end
+  end
+
+  def partgen(dest)
+    dest2 = dest + ".part"
+    yield dest2
+    File.rename dest2, dest
   end
 
   def image_dir
@@ -29,20 +66,60 @@ class ImageSet
     end
     path = @dir + "/" + filename
     return path if File.exist?(path)
-    case filename
-    when /\Aout/
-      raise ArgumentError, "cannot generate source image: #{filename.inspect}"
-    when %r{\Abb-thum(.*)\.png\z}
-      out_filename = @dir + "/out#{$1}.tiff"
-      thum_filename = @dir + "/#{filename}"
-      Open3.pipeline(
-        ['tifftopnm', out_filename, :err => "/dev/null"],
-        "pnmscale -width=80",
-        ['pnmtopng', :out => thum_filename])
-    else
-      raise ArgumentError, "unexpected filename: #{filename.inspect}"
+    rule = @index_hash["i"][filename]
+    if !rule
+      raise ArgumentError, "no rule for #{filename.inspect}"
     end
+    run(rule, path)
     path
+  end
+
+  def run(rule, dest)
+    #p [rule, dest]
+    case rule[0]
+    when 'make'
+      path0 = make(rule[1])
+      partgen(dest) {|dest2|
+        FileUtils.cp path0, dest2
+      }
+      {}
+    when "flip"
+      mktmppath {|tmppath|
+        h = run(rule[2], tmppath)
+        partgen(dest) {|dest2|
+          system('pnmflip', "-"+rule[1], tmppath, :out => dest2)
+        }
+        h
+      }
+    when "resolution"
+      dpi = rule[1]
+      h = run(rule[2], dest)
+      h['dpi'] = dpi
+      h
+    when "convert"
+      case rule[1]
+      when 'png'
+        mktmppath {|tmppath|
+          partgen(dest) {|dest2|
+            h = run(rule[2], tmppath)
+            commandline = ['pnmtopng']
+            if h['dpi']
+              # dot/inch -> dot/meter
+              dpm = (h['dpi'] / 25.4 * 1000).round
+              commandline.concat ['-phys', dpm.to_s, dpm.to_s, '1']
+            end
+            commandline << tmppath
+            commandline << {:out => dest2}
+            system(*commandline)
+            h
+          }
+        }
+      else
+        raise ArgumentError, "unexpected format: #{rule[1].inspect}"
+      end
+    else
+      raise ArgumentError, "unexpected rule type: #{rule[0].inspect}"
+    end
   end
 end
 
