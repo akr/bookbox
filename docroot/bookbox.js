da2ea43ad185feb2445c736a6e301dcdbfee3bb4
Tanaka Akira <akr@fsij.org>
2011-04-09 17:41:06 +0900

    add a link to change all thumbnail colors at once.

--- docroot/bookbox.js
+++ docroot/bookbox.js
@@ -1,5 +1,5 @@
-function change_thumbnail(name) {
-  var i;
+function change_thumbnail(stem) {
+  name = "img" + stem;
   var src = document[name].src;
   if (src.match(/_c\.png$/))
     document[name].src = src.replace(/_c\.png$/, '_g.png');
@@ -7,4 +7,12 @@ function change_thumbnail(name) {
     document[name].src = src.replace(/_g\.png$/, '_m.png');
   else if (src.match(/_m\.png$/))
     document[name].src = src.replace(/_m\.png$/, '_c.png');
+}
+
+function thumbnail_all_color(stems, ch) {
+  var suffix = '_' + ch + '.png';
+  for (var i in stems) {
+    name = "img" + stems[i];
+    document[name].src = document[name].src.replace(/_[cgm]\.png$/, suffix);
+  }
 }
