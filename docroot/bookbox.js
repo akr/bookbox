209fe0b114e21f405c07b5ca0a78128e5d33eedb
Tanaka Akira <akr@fsij.org>
2011-04-09 17:45:53 +0900

    set_thumbnail_color_mode extracted.

--- docroot/bookbox.js
+++ docroot/bookbox.js
@@ -1,18 +1,22 @@
 function change_thumbnail(stem) {
-  name = "img" + stem;
+  var name = "img" + stem;
   var src = document[name].src;
   if (src.match(/_c\.png$/))
-    document[name].src = src.replace(/_c\.png$/, '_g.png');
+    set_thumbnail_color_mode(stem, 'g');
   else if (src.match(/_g\.png$/))
-    document[name].src = src.replace(/_g\.png$/, '_m.png');
+    set_thumbnail_color_mode(stem, 'm');
   else if (src.match(/_m\.png$/))
-    document[name].src = src.replace(/_m\.png$/, '_c.png');
+    set_thumbnail_color_mode(stem, 'c');
 }
 
-function thumbnail_all_color(stems, ch) {
-  var suffix = '_' + ch + '.png';
+function thumbnail_all_color(stems, mode) {
   for (var i in stems) {
-    name = "img" + stems[i];
-    document[name].src = document[name].src.replace(/_[cgm]\.png$/, suffix);
+    set_thumbnail_color_mode(stems[i], mode);
   }
+}
+
+function set_thumbnail_color_mode(stem, mode) {
+  var name = "img" + stem;
+  var suffix = '_' + mode + '.png';
+  document[name].src = document[name].src.replace(/_[cgm]\.png$/, suffix);
 }
