95a5c7b6f3b6e552ec47c5b5ad222d6905375415
Tanaka Akira <akr@fsij.org>
2011-04-24 04:22:10 +0900

    forgot to add.

---
+++ lib/bookbox/main/imagedir2pdf.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+require_relative 'images2pdf'
+
+def op_imagedir2pdf
+  op = OptionParser.new
+  op.def_option('--verbose', 'verbose mode') { $opt_verbose = true }
+  op
+end
+
+def main_imagedir2pdf(argv)
+  op = op_imagedir2pdf
+  op.parse!(argv)
+
+  argv.each {|arg|
+    main_images2pdf([arg, '-o', "#{arg}.pdf"])
+  }
+end
