252e0edeeec0a637ef0faa2104884ada0b39a0dd
Tanaka Akira <akr@fsij.org>
2011-05-08 01:32:45 +0900

    implement bookbox make -s option.

--- lib/bookbox/main/make.rb
+++ lib/bookbox/main/make.rb
@@ -1,12 +1,14 @@
 require 'optparse'
 
 $opt_verbose = 0
+$opt_suffix = ''
 
 def op_make
   op = OptionParser.new
   op.banner = 'Usage: bookbox make target...'
   op.def_option('-h', '--help', 'show help message') { puts op; exit 0 }
   op.def_option('--verbose', 'verbose mode') { $opt_verbose += 1 }
+  op.def_option('-s SUFFIX', '--suffix=SUFFIX', 'specify suffix') {|arg| $opt_suffix = arg }
   op
 end
 
@@ -15,6 +17,7 @@ def main_make(argv)
   im = BookBox::ImageMaker.new
   im.verbose = $opt_verbose
   argv.each {|arg|
+    arg += $opt_suffix
     # File.realpath and File.realdirpath is not usable because directories may not be exist.
     pp im.make File.expand_path(arg)
   }
